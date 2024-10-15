e8ed76858f2ead66cc39dff76ae2ae7af45c3e70
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-15 10:30:34 +0300

    Excellent Result
    Excellent Result

---
+++ 01. Excellent Result.py
@@ -0,0 +1,10 @@
+def evaluate_grade(grade):
+    if grade >= 5.5:
+        return "Excellent!"
+
+grade = float(input())
+result = evaluate_grade(grade)
+
+if result is not None:
+    print(result)
+    
