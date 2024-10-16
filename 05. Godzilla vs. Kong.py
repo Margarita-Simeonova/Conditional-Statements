6a41c0f7d3c9606f3830f70a91ca0365c1421668
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-16 18:11:22 +0300

    Godzilla vs. Kong
    Godzilla vs. Kong

---
+++ 05. Godzilla vs. Kong.py
@@ -0,0 +1,19 @@
+def godzilla_vs_kong(budget, extras_count, clothing_price):
+
+    decor = budget * 0.1
+
+    if extras_count > 150:
+        clothing_price *= 0.9
+
+    total_price = decor + (clothing_price * extras_count)
+
+    if total_price <= budget:
+        return f"Action!\nWingard starts filming with {budget - total_price:.2f} leva left."
+    else:
+        return f"Not enough money!\nWingard needs {total_price - budget:.2f} leva more."
+
+
+money = float(input())
+extras = int(input())
+price_of_clothing = float(input())
+print(godzilla_vs_kong(money, extras, price_of_clothing))
