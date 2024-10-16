f9cf05b860f26d30bd598b1be6f2d204a9942e82
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-16 18:06:44 +0300

    Bonus Score
    Bonus Score

---
+++ 02. Bonus Score.py
@@ -0,0 +1,21 @@
+def bonus_score(number):
+
+    result = 0
+
+    if number <= 100:
+        result += 5
+    elif number > 1000:
+        result += number * 0.1
+    elif number > 100:
+        result += number * 0.2
+
+    if number % 2 == 0:
+        result += 1
+    elif number % 10 == 5:
+        result += 2
+
+    return f"{result}\n{result + number}"
+
+
+numb = int(input())
+print(bonus_score(numb))
