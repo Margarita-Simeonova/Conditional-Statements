e6e5314f25703742bae8c7ef4e640b2fd2ea1460
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-16 17:54:54 +0300

    Greater Number
    Greater Number

---
+++ 02. Greater Number.py
@@ -0,0 +1,12 @@
+def greater_number(numb_1, numb_2):
+
+    if numb_1 > numb_2:
+        return numb_1
+
+    else:
+        return numb_2
+
+
+first_number = int(input())
+second_number = int(input())
+print(greater_number(first_number, second_number))
