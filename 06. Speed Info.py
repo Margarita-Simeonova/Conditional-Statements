bb7beaf96b54f347444d55f351d1a2a3b47975ab
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-16 18:01:55 +0300

    Speed Info
    Speed Info

---
+++ 06. Speed Info.py
@@ -0,0 +1,16 @@
+def speed_info(speed):
+
+	if speed <= 10:
+		return "slow"
+	elif 10 < speed <= 50:
+		return "average"
+	elif 50 < speed <= 150:
+		return "fast"
+	elif 150 < speed <= 1000:
+		return "ultra fast"
+	elif speed > 1000:
+		return "extremely fast"
+
+
+speed = float(input())
+print(speed_info(speed))
