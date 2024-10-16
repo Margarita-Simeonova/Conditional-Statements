7ae7b2aa267e7ca319d28de9f2c6054afd141e09
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-16 18:12:32 +0300

    World Swimming Record
    World Swimming Record

---
+++ 06. World Swimming Record.py
@@ -0,0 +1,21 @@
+import math
+
+
+def world_swimming_record(best_time, distance, time_per_meter):
+
+    swimming_time = distance * time_per_meter
+    delay = (math.floor(distance / 15)) * 12.5
+    total_time = swimming_time + delay
+
+    difference = abs(total_time - best_time)
+
+    if total_time < best_time:
+        return f"Yes, he succeeded! The new world record is {total_time:.2f} seconds."
+    else:
+        return f"No, he failed! He was {difference:.2f} seconds slower."
+
+
+record = float(input())
+metres = float(input())
+time = float(input())
+print(world_swimming_record(record, metres, time))
