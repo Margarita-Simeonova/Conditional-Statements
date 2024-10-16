bf4c45fefe9cae96b8a1661e2972e1a014c87436
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-16 18:03:28 +0300

    Area of Figures
    Area of Figures

---
+++ 07. Area of Figures.py
@@ -0,0 +1,29 @@
+import math
+
+
+def area_of_figure(figure):
+    result = 0
+
+    if figure == "square":
+        site = float(input())
+        result = site ** 2
+
+    elif figure == "rectangle":
+        site_a = float(input())
+        site_b = float(input())
+        result = site_a * site_b
+
+    elif figure == "circle":
+        radius = float(input())
+        result = math.pi * (radius ** 2)
+
+    elif figure == "triangle":
+        site = float(input())
+        h = float(input())
+        result = (site * h) / 2
+
+    return f"{result:.3f}"
+
+
+figure_arg = input()
+print(area_of_figure(figure_arg))
