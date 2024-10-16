c0244eaa33bf2900fa487846b96eb36b6b71bae2
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-16 18:00:48 +0300

    Number 100...200
    Number 100...200

---
+++ 05. Number 100...200.py
@@ -0,0 +1,13 @@
+def number_100_200(numb):
+
+    if numb < 100:
+        return "Less than 100"
+    elif numb > 200:
+        return "Greater than 200"
+
+    else:
+        return "Between 100 and 200"
+
+
+number = int(input())
+print(number_100_200(number))
