ccf5b90dd71ff1c19660823ca4667403739a6538
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-12-03 15:15:38 +0200

    Update 03. Time + 15 Minutes.py

--- 03. Time + 15 Minutes.py
+++ 03. Time + 15 Minutes.py
@@ -1,11 +1,15 @@
 def time_15_min(hours, minutes):
+    # def time plus 15 minutes
 
+    # make hours to minutes
     time_in_minutes = (hours * 60)
     time = time_in_minutes + minutes + 15
 
+    # reverse minutes to hours
     total_time_in_hours = time // 60
     total_time_in_minutes = time % 60
 
+    # if statrmant
     if total_time_in_hours >= 24:
         total_time_in_hours = 0
 
@@ -13,6 +17,7 @@ def time_15_min(hours, minutes):
         total_time_in_hours += 1
         total_time_in_minutes = 0
 
+    # return result
     if total_time_in_minutes < 10:
         return f"{total_time_in_hours}:0{total_time_in_minutes}"
     else:
