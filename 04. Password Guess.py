6be87a5843ab5d068fcbca238e2e1882137ab69d
Margarita Simeonoff <90966716+Margarita-Simeonova@users.noreply.github.com>
2024-12-04 11:23:33 +0200

    Update 04. Password Guess.py

--- 04. Password Guess.py
+++ 04. Password Guess.py
@@ -1,12 +1,14 @@
 def password_guess(password):
-
+    # difine function
     if password == "s3cr3t!P@ssw0rd":
         return "Welcome"
     else:
         return "Wrong password!"
 
 
+# input
 text = input()
+# result
 result = password_guess(text)
 
 print(result)
